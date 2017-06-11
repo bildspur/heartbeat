3df326829562dabb62ad55eb667dc27d55f072bc
Florian Bruggisser <florian@nexpose.ch>
2017-06-11 22:27:54 +0200

    changed screens leep configuration

--- install/install-dep.sh
+++ install/install-dep.sh
@@ -8,3 +8,6 @@ sudo apt-get install --yes gstreamer1.0-omx
 # add autorun
 sudo chmod +x /home/pi/heartbeat-master/run.sh
 sudo echo "@/home/pi/heartbeat-master/run.sh" >> /etc/xdg/lxsession/LXDE/autostart
+
+# change screen config (no sleep)
+/bin/cp -rf screen/lightdm.conf /etc/lightdm/lightdm.conf
