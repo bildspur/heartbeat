2fd6f170b7c7d8addb03ad132cc230daa314e0d2
Florian Bruggisser <florian@nexpose.ch>
2017-06-20 01:27:33 +0200

    tried to fix update script (still not working because github is not reachable)

--- install/update.sh
+++ install/update.sh
@@ -3,24 +3,28 @@
 # create update folder
 if [ -d /home/pi/update ]
 	  then
-    echo "udpate folder exists."
-    rm -r /home/pi/update
+    echo "update folder exists..."
+    sudo rm -rf /home/pi/update
+    echo "removed!"
 fi
-mkdir /home/pi/update
+
+sudo mkdir /home/pi/update
+sudo chmod 777 /home/pi/update
 
 # change to this folder
 cd /home/pi/update
 
 # check if github is available
-if ping -c 1 github.com &> /dev/null
+if ping -c 1 192.30.253.113 &> /dev/null
 then
   echo "Github found! Updating..."
 else
+  echo "Github not reachable!"
   exit 1
 fi
 
 # download last files from github
-wget -qO- https://github.com/bildspur/heartbeat/archive/master.zip | jar xvf /dev/stdin
+sudo wget -qO- https://github.com/bildspur/heartbeat/archive/master.zip | jar xvf /dev/stdin
 
 if [ -d ./heartbeat-master ]
   then
